00fa1cd7b3c39d9beefa9bbf5debe29270936284
BenJCross1995 <92743455+BenJCross1995@users.noreply.github.com>
2023-02-20 17:57:09 +0000

    Update Chapter 02 - Getting Started with ggplot2.R

--- Chapter 02 - Getting Started with ggplot2.R
+++ Chapter 02 - Getting Started with ggplot2.R
@@ -5,8 +5,25 @@
 #--------------------CHAPTER 2--------------------#
 #-------------------------------------------------#
 
-#----2.2 FUEL ECONOMY DATA----
+#----2.1 FUEL ECONOMY DATA----
 
+#----Code Chunk 2.1
 library(ggplot2)
 
 mpg
+
+#----2.3 KEY COMPONENTS----
+
+#----Code Chunk 2.2
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point()
+
+#----2.4 COLOUR, SIZE, SHAPE AND OTHER AESTHETIC ATTRIBUTES----
+
+#----Code Chunk 2.3
+ggplot(mpg, aes(displ, cty, colour = class)) +
+  geom_point()
+
+#----Code Chunk 2.4
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(colour = "blue")
